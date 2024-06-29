ac0bb540acc15f88331c096106a43f63a131353e
Vikash Kumawat <75132736+vikashjalandra@users.noreply.github.com>
2024-06-29 12:14:10 +0530

    custom

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,12 +1,12 @@
 import Chai from "./chai"
 
 function App() {
-
+  const username ='chai or code'
 
   return (
     <>
       <Chai />
-      <h1>chai</h1>
+      <h1>chai {username}</h1>    {/* {} -- is evaluated expression */}
     </>
   )
 }
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,8 +2,37 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp() {
+  return(
+    <div>
+      <h1>Cusrom App</h1>
+    </div>
+  )
+}
+
+// const ReactElement = {
+//   type:'a',
+//   props:{
+//       href:'https://google.com',
+//       target:'_blank'
+//   },
+//   children:'Click me to visit google'
+// }
+
+const anotheUser ='hello world'
+
+const reactElement =React.createElement(
+  'a',
+  {href:'https://google.com', target:'_blank'},
+  'Click Me to Visit Google',
+  anotherElement
+)
+
+const anotherElement=(
+  <a href="https://google.com" target='_blank'>Visit Google</a>
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  reactElement
+
 )
