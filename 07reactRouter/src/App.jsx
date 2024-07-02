8bbf70bdb389c1f37ce70631625acbc851de0eef
Vikash Kumawat <75132736+vikashjalandra@users.noreply.github.com>
2024-07-02 21:08:21 +0530

    new

---
+++ 07reactRouter/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+    <h1 className="bg-green-500 p-4 text-white">Hello World</h1>
+      </>
+  )
+}
+
+export default App
